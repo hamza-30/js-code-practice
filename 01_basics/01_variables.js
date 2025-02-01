8928808e0c2a7e767bbb82168c7645999576a031
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-01 09:40:23 +0000

    variable keywords in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14455
+let accountEmail = "hamza@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur";
+
+//accountId = 2
+accountEmail = "hamza212@gmail.com"
+accountPassword = "2342"
+accountCity = "Chicago"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
