d170223d8642f252f312f99ba12f91726ee57920
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-02 09:57:09 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1 )
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined < 0)
+
+
+// === (strict check) checks if the two expressions are of same data 
+// type and then compares. If data type is not same, returns false.
+console.log("2" === 2)
