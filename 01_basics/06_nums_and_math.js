d6b96d7d6b92a3fdeb5247cf84a4a7ae1c2e5469
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-03 08:32:47 +0000

    numbers and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(typeof balance)
+
+// console.log(balance.toFixed(1))
+
+const otherNumber = 224.785332
+
+// console.log(otherNumber.toPrecision(2))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+
+
+//+++++++++++++++++++MATHS++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.5))
+// console.log(Math.ceil(35.3))
+// console.log(Math.floor(7.6))
+// console.log(Math.min(34,23,52,2,6,3,52,1))
+// console.log(Math.max(2,4,6,2,6,32,64))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min) + 1) + min)
