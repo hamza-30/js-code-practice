1a9239cb704092fd6f090547dae0c9f3694d32e3
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-05 09:14:06 +0000

    immediately invoked functions

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+//Immediately Invoked Function Expressions
+
+//To prevent the pollution of global scope declarations, immediately invoked functions are used
+
+//immediately invoked functions are executed immediately after defined
+
+(function greeting() {
+    console.log("hello world")
+})()
