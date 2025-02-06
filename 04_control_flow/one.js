dd7b6a581644e1c7d555c5dfecc7fa7a3e4f8df6
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-06 07:25:04 +0000

    if else statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// if
+// const isUserLoggedIn = true;
+// const temperature = 41
+
+// if (temperature === 40){
+//     console.log("less than 50")
+// }
+// else{
+// console.log("temperature > 50")
+// }
+// <, >, <=, >= , ==, !=, ===
+
+const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power: ${power}`)
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500")
+//     console.log("less than 750")
+// } else if (balance < 900){
+//     console.log("less than 900")
+// } else {
+//     console.log("less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2){ 
+    console.log("Allow to buy course")
+}
+
+if(loggedInFromGoogle || loggedInFromEmail ){
+    console.log("User logged in")
+}
