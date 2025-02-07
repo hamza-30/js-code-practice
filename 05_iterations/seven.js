93eab9551f42a2aacf88a9155d4a011c32397440
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-07 05:51:13 +0000

    map method with filter chaining

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,2 +1,41 @@
 //map
 //map and filter chaining
+
+const array = [32,53,123,653,73,235,2]
+const values = array.map( (item) => {
+    return (item / 2)
+} )
+
+console.log(values)
+
+const array2 = [53,66,23,85,345,34,67,23]
+const newArray = array2.map( (element) => {
+    return element + 1
+}).map( (element) => {
+   return element + 2
+}).filter( (element) => {
+    return (element % 2) == 0
+})
+
+console.log(newArray)
+
+const arraywithObj = [
+    {
+    name: "Hamza",
+    age: 20,
+    semester: "5th",
+    university: "University of Karachi"
+    },
+    {
+    name: "Tuaha",
+    age: 25,
+    semester: "8th",
+    university: "Bahria University"
+    }
+]
+
+const arrayValue = arraywithObj.map((obj) => {
+    return `${obj.name} ${obj.age}`
+})
+
+console.log(arrayValue)
