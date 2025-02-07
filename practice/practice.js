18c149ae2c440cf562175ea6a0193d755fc41ab8
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-02-07 06:19:41 +0000

    JS practice of basics

---
+++ practice/practice.js
@@ -0,0 +1,10 @@
+const array = ['H', 'A', 'M', 'Z', 'A']
+// const newArray = []
+// array.forEach((item)=> {
+//     newArray.push(item)
+// })
+
+// console.log(newArray)
+
+let values = array.join('')
+console.log(values)
