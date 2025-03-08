75330697a2230b5c88e745c8be62f7f75d1fa676
hamza-30 <129505018+hamza-30@users.noreply.github.com>
2025-03-08 20:07:00 +0000

    getter setter in class

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,28 @@
+class User {
+    constructor(email, password){
+        this.email = email
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+
+const hamza = new User("hamza@google.com", "abc")
+console.log(hamza.password)
+console.log(hamza.email)
+
